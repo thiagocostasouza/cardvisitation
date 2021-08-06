d8fd35fd629557aa2602a8d2528bb9fd1e863496
thiago <thiago.souza@invillia.com>
2021-08-06 20:21:32 -0300

    first commit

---
+++ app/src/main/java/com/example/cartodevisita/App.kt
@@ -0,0 +1,7 @@
+package com.example.cartodevisita
+
+import android.app.Application
+
+class App : Application() {
+
+}
---
+++ app/src/main/java/com/example/cartodevisita/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.cartodevisita.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cartodevisita.R
+import com.example.cartodevisita.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
